eafee720d0e0a57223f9e24008cea7aacb2fd099
Ivan Vorobei <hello@ivanvorobei.io>
2020-09-24 17:01:26 +0300

    Add basic targets

--- Package.swift
+++ Package.swift
@@ -1,19 +1,17 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SparrowKit",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v12), .tvOS(.v12), .watchOS(.v6)
     ],
     products: [
         .library(name: "SparrowKit", targets: ["SparrowKit"])
     ],
     dependencies: [],
     targets: [
-        .target(name: "SparrowKit", dependencies: []),
-        .testTarget(name: "SparrowKitTests", dependencies: ["SparrowKit"])
+        .target(name: "SparrowKit", dependencies: [])
     ]
 )
