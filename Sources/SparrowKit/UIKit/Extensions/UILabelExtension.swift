dbef43407184fba762966cb09e0a397d58d927f1
Ivan Vorobei <hello@ivanvorobei.io>
2020-12-03 22:05:18 +0300

    Update layoutDynamicHeight

--- Sources/SparrowKit/UIKit/Extensions/UILabelExtension.swift
+++ Sources/SparrowKit/UIKit/Extensions/UILabelExtension.swift
@@ -61,7 +61,7 @@ public extension UILabel {
     func layoutDynamicHeight(x: CGFloat, y: CGFloat, width: CGFloat) {
         frame = CGRect.init(x: x, y: y, width: width, height: frame.height)
         sizeToFit()
-        if frame.width < width {
+        if frame.width != width {
             frame.setWidth(width)
         }
     }
