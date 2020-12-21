e895d75f1aec0307a71e0ae97ce8df147699c08e
Alexandr Guzenko <alxrguz@icloud.com>
2020-12-21 10:07:03 +0300

    New extension for UIStackView

--- Sources/SparrowKit/UIKit/Extensions/UIStackViewExtension.swift
+++ Sources/SparrowKit/UIKit/Extensions/UIStackViewExtension.swift
@@ -1,8 +1,41 @@
+// The MIT License (MIT)
+// Copyright © 2020 Ivan Vorobei (hello@ivanvorobei.by)
 //
-//  File.swift
-//  
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//  Created by Alexandr Guzenko on 21.12.2020.
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
-import Foundation
+import UIKit
+
+extension UIStackView {
+    /**
+     SparrowKit: Add many arranged subviews as array.
+     
+     - parameter subviews: Array of `UIView` objects.
+     */
+    func addArrangedSubviews(_ subviews: [UIView]) {
+        subviews.forEach { addArrangedSubview($0) }
+    }
+    
+    /**
+     SparrowKit: Remove all arranged subviews.
+     */
+    func removeArrangedSubviews() {
+        arrangedSubviews.forEach { $0.removeFromSuperview() }
+    }
+}
+
