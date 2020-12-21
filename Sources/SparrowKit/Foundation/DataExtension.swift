68ba5a6a65c035ad4503d6e8d1750bf8a2afc160
Alexandr Guzenko <alxrguz@icloud.com>
2020-12-21 10:06:39 +0300

    New methods for Codable protocol

--- Sources/SparrowKit/Foundation/DataExtension.swift
+++ Sources/SparrowKit/Foundation/DataExtension.swift
@@ -20,56 +20,13 @@
 // SOFTWARE.
 
 import Foundation
-#if !os(Linux)
-  import CoreGraphics
-#endif
-#if os(iOS) || os(tvOS)
-  import UIKit.UIGeometry
-#endif
 
-public protocol Do {}
-
-extension Do where Self: Any {
+extension Data {
     /**
-     SparrowKit: Synaxic sugar for code reduction. Access instance or object properties using `do` via closures
-     ```
-     let titleLabel = UILabel().do {
-         $0.font = .systemFont(ofSize: 34, weight: .bold)
-         $0.textColor = .systemRed
-     }
-     ```
-        --- or ---
-     ```
-     let titleLabel = UILabel()
-     titleLabel.do {
-         $0.font = .systemFont(ofSize: 34, weight: .bold)
-         $0.textColor = .systemRed
-     }
-     ```
+    SparrowKit: Decodes data to Decodable model
      */
-    @discardableResult @inlinable
-    public func `do`(_ block: (Self) throws -> Void) rethrows -> Self {
-        try block(self)
-        return self
+    func decoded<T: Decodable>(type: T.Type) throws -> T? {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: self)
     }
-    
 }
-
-extension NSObject: Do {}
-
-#if !os(Linux)
-  extension CGPoint: Do {}
-  extension CGRect: Do {}
-  extension CGSize: Do {}
-  extension CGVector: Do {}
-#endif
-
-extension Array: Do {}
-extension Dictionary: Do {}
-extension Set: Do {}
-
-#if os(iOS) || os(tvOS)
-  extension UIEdgeInsets: Do {}
-  extension UIOffset: Do {}
-  extension UIRectEdge: Do {}
-#endif
