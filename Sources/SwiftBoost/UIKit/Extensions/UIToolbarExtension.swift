e98030f694861986821d5f4dce1a0c9273924858
Ivan Vorobei <hello@ivanvorobei.io>
2023-03-22 21:00:24 +0300

    Fix tv os compability.

--- Sources/SwiftBoost/UIKit/Extensions/UIToolbarExtension.swift
+++ Sources/SwiftBoost/UIKit/Extensions/UIToolbarExtension.swift
@@ -1,4 +1,4 @@
-#if canImport(UIKit) && (os(iOS) || os(tvOS))
+#if canImport(UIKit) && (os(iOS))
 import UIKit
 
 extension UIToolbar {
@@ -6,10 +6,10 @@ extension UIToolbar {
     /**
      SwiftBoost: Set appearance for tab bar.
      */
-    @available(iOS 13.0, tvOS 13.0, *)
+    @available(iOS 13.0, *)
     public func setAppearance(_ value: ToolbarAppearance) {
         self.standardAppearance = value.standardAppearance
-        if #available(iOS 15.0, tvOS 15.0, *) {
+        if #available(iOS 15.0, *) {
             self.scrollEdgeAppearance = value.scrollEdgeAppearance
         }
     }
@@ -17,7 +17,7 @@ extension UIToolbar {
     /**
      SwiftBoost: Appearance cases.
      */
-    @available(iOS 13.0, tvOS 13.0, *)
+    @available(iOS 13.0, *)
     public enum ToolbarAppearance {
         
         case transparentAlways
