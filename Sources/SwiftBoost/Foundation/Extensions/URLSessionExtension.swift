890ed0782b18baf1e2bafdfbbb8369faa105e5d2
Ivan Vorobei <hello@ivanvorobei.io>
2025-05-21 11:39:01 +0300

    Updated network worker.

--- Sources/SwiftBoost/Foundation/Extensions/URLSessionExtension.swift
+++ Sources/SwiftBoost/Foundation/Extensions/URLSessionExtension.swift
@@ -12,9 +12,9 @@ public extension URLSession {
         public func errorMessage() -> String {
             switch self {
             case .invalidURL(let str):
-                return "badURL: \(str)"
+                return "bad URL: \(str)"
             case .networkError(let error):
-                return "networkError: \(error)"
+                return "network Error: \(error)"
             case .noResponse:
                 return "no network response"
             case .decodingError(let error):
@@ -26,10 +26,28 @@ public extension URLSession {
     enum HTTPMethod {
         
         case get
+        case post
+        case put
+        case delete
         
         var id: String {
             switch self {
             case .get: return "get"
+            case .post: return "post"
+            case .put: return "put"
+            case .delete : return "delete"
+            }
+        }
+    }
+    
+    enum ContentType {
+        
+        case application_json
+        
+        var id: String {
+            switch self {
+            case .application_json:
+                "application/json"
             }
         }
     }
@@ -37,6 +55,8 @@ public extension URLSession {
     static func request(
         url: String,
         method: HTTPMethod,
+        body: [String: Any]? = nil,
+        contentTypeHeader: ContentType? = nil,
         completion: @escaping (AppError?, Data?, HTTPURLResponse?) ->Void)
     {
         guard let url = URL(string: url) else {
@@ -46,6 +66,24 @@ public extension URLSession {
         
         var request = URLRequest(url: url)
         request.httpMethod = method.id
+        
+        // Content Type
+        if let contentTypeHeader {
+            request.setValue(contentTypeHeader.id, forHTTPHeaderField: "Content-Type")
+        }
+        
+        // Body
+        if let body {
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: body, options: [])
+                request.httpBody = jsonData
+            } catch {
+                completion(.decodingError(error), nil, nil)
+                return
+            }
+        }
+        
+        // Make Request
         URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard let response = response as? HTTPURLResponse else {
                 completion(AppError.noResponse, nil, nil)
